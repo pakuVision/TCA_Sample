8fa9ed7c79b9bb7eb49643ecd9c533d77eafbb94
pakuVision <boardguy.vision@gmail.com>
2025-10-31 11:16:20 +0900

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by boardguy.vision on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
